09cf2541c8efcfda77e88f30b81a1b054cb9ae6e
KEWEN SILVA <kewensilva58@gmail.com>
2024-08-10 20:03:00 -0300

    [feat: verifica_congelado]

--- geracao_particoes.c
+++ geracao_particoes.c
@@ -80,12 +80,28 @@ void classificacao_interna(char *nome_arquivo_entrada, Nomes *nome_arquivos_said
     -> Caso seja menor, guarda no vetor, na posição indicada, o dado;
     -> Caso não, retorna 0.
 */
-int congela(int *vetor, int dado, int *pos, int m) {
-    if (pos < m)
+int congela(int *vetor, int dado, int *tam, int max) {
+    if (tam < max)
     {
-        vetor[(*pos)++] = dado;
+        vetor[(*tam)++] = dado;
         return 1;
     }
+    return 0;
+}
+
+/*------Verifica se a posição está congelada------*/
+/*
+    -> Recebe o vetor onde estão sendo guardados os elementos que vão ser ignorados;
+    -> Recebe a posição e o tamanho do vetor;
+    -> Recebe o código atual do loop exterior;
+    -> Verifica se código está congelado;
+    -> Caso esteja, retorna 1;
+    -> Caso não, retorna 0.
+*/
+int verifica_congelado(int *vetor, int *tam, int cod_atual) {
+    for (int i = 0; i < tam; i++)
+        if (vetor[i] == cod_atual)
+            return 1;
     return 0;
 }
 
@@ -93,8 +109,8 @@ int congela(int *vetor, int dado, int *pos, int m) {
 /*
     -> Recebe o dado do arquivo e compara com o último dado inserido na partição;
     -> Basicamente, é retornado um inteiro que indica se o dado é menor ou não;
-    -> Esse método pode ser utilizado tanto na etapa antes do congelamento que é caso do seleção com substituição,
-    quanto para inserir no reservatório no caso da seleção naturol.
+    -> Esse método pode ser utilizado tanto na etapa antes do congelamento que é caso
+    do seleção com substituição, quanto para inserir no reservatório no caso da seleção naturol.
 */
 int compara_arq_memoria(int dado, int memoria) {
     return dado < memoria ? 1 : 0;
@@ -102,6 +118,15 @@ int compara_arq_memoria(int dado, int memoria) {
 
 void selecao_com_substituicao(char *nome_arquivo_entrada, Nomes *nome_arquivos_saida, int M)
 {    
+    int dado, memoria;
+    int *vetor, tam;
+    vetor = (int*) malloc(sizeof(int)*M);
+    // uso
+    if (compara_arq_memoria(dado, memoria))
+    {
+        congela(vetor, dado, &tam, M);
+    }
+        
 	//TODO: Inserir aqui o codigo do algoritmo de geracao de particoes
 }
 
