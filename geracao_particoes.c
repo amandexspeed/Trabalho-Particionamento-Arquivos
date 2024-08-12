176d375ac991e7ba043a694cab59d867992b4aee
Amanda Melo <60000299+amandexspeed@users.noreply.github.com>
2024-08-12 18:34:46 -0300

    Equipe 1 (#4)
    * adicionando funções auxiliares
    
    * Add função carrega_registros
    
    ---------
    
    Co-authored-by: Fernanda Mirely <safiraaabarbosaaa@gmail.com>
    Co-authored-by: Rafael S. Lima <rafaelkra2@gmail.com>

--- geracao_particoes.c
+++ geracao_particoes.c
@@ -10,65 +10,123 @@
 #include "geracao_particoes.h"
 #include "nomes.h"
 #include "cliente.h"
+#include "limits.h"
 
 void classificacao_interna(char *nome_arquivo_entrada, Nomes *nome_arquivos_saida, int M)
 {
-	int fim = 0; //variável de controle para saber se arquivo de entrada terminou
-    FILE *arq; //declara ponteiro para arquivo
+  int fim = 0; // variável de controle para saber se arquivo de entrada terminou
+  FILE *arq;   // declara ponteiro para arquivo
 
-    //abre arquivo para leitura
-    if ((arq = fopen(nome_arquivo_entrada, "rb")) == NULL) {
-        printf("Erro ao abrir arquivo de entrada\n");
-    } else {
+  // abre arquivo para leitura
+  if ((arq = fopen(nome_arquivo_entrada, "rb")) == NULL)
+  {
+    printf("Erro ao abrir arquivo de entrada\n");
+  }
+  else
+  {
 
-        //le primeiro cliente
-        Cliente *cin = le_cliente(arq);
+    // le primeiro cliente
+    Cliente *cin = le_cliente(arq);
 
-        while (!(fim)) {
-            //le o arquivo e coloca no vetor
-            Cliente *v[M];
+    while (!(fim))
+    {
+      // le o arquivo e coloca no vetor
+      Cliente *v[M];
 
-            int i = 0;
-            while (!feof(arq) && i < M) {
-                v[i] = cin;
-                cin = le_cliente(arq);
-                i++;
-            }
+      int i = 0;
+      while (!feof(arq) && i < M)
+      {
+        v[i] = cin;
+        cin = le_cliente(arq);
+        i++;
+      }
 
-            //ajusta tamanho M caso arquivo de entrada tenha terminado antes do vetor
-            if (i != M) {
-                M = i;
-            }
+      // ajusta tamanho M caso arquivo de entrada tenha terminado antes do vetor
+      if (i != M)
+      {
+        M = i;
+      }
 
-            //faz ordenacao
-            for (int j = 1; j < M; j++) {
-                Cliente *c = v[j];
-                i = j - 1;
-                while ((i >= 0) && (v[i]->cod_cliente > c->cod_cliente)) {
-                    v[i + 1] = v[i];
-                    i = i - 1;
-                }
-                v[i + 1] = c;
-            }
-
-            //cria arquivo de particao e faz gravacao
-            char *nome_particao = nome_arquivos_saida->nome;
-            nome_arquivos_saida = nome_arquivos_saida->prox;
-
-            FILE *p;
-            if ((p = fopen(nome_particao, "wb")) == NULL) {
-                printf("Erro criar arquivo de saida\n");
-            } else {
-                for (int i = 0; i < M; i++) {
-					salva_cliente(v[i], p);
-                }
-                fclose(p);
-            }
-            if (feof(arq)) {
-                fim = 1;
-            }
+      // faz ordenacao
+      for (int j = 1; j < M; j++)
+      {
+        Cliente *c = v[j];
+        i = j - 1;
+        while ((i >= 0) && (v[i]->cod_cliente > c->cod_cliente))
+        {
+          v[i + 1] = v[i];
+          i = i - 1;
         }
+        v[i + 1] = c;
+      }
+
+      // cria arquivo de particao e faz gravacao
+      char *nome_particao = nome_arquivos_saida->nome;
+      nome_arquivos_saida = nome_arquivos_saida->prox;
+
+      FILE *p;
+      if ((p = fopen(nome_particao, "wb")) == NULL)
+      {
+        printf("Erro criar arquivo de saida\n");
+      }
+      else
+      {
+        for (int i = 0; i < M; i++)
+        {
+          salva_cliente(v[i], p);
+        }
+        fclose(p);
+      }
+      if (feof(arq))
+      {
+        fim = 1;
+      }
     }
+  }
+}
+
+// Guarda no arquvio os dados do vetor e o zera
+void guarda_no_arquivo(Cliente **v, int i, FILE *p)
+{
+  int menor = procuraMenor(v, i);
+  while (menor != INT_MAX)
+  {
+    salva_cliente(v[menor], p);
+    v[menor] = cliente(INT_MAX, "");
+    menor = procuraMenor(v, i);
+  }
+}
+
+void cria_particao(char *nome_particao, Nomes *nome_arquivos_saida, Cliente **v, FILE *p, int requisita_funcao, int menor, int i)
+{
+  if ((p = fopen(nome_particao, "wb")) == NULL)
+  {
+    printf("Erro criar arquivo de saida\n");
+  }
+  else
+  {
+    if (requisita_funcao == 1)
+      guarda_no_arquivo(v, i, p);
+
+    fclose(p);
+  }
+}
+
+// carregar vetor com os M registros
+void carrega_registros(Cliente *v[], FILE *arq, int M)
+{
+  Cliente *cin = le_cliente(arq);
+  Cliente *v[M];
+  int i = 0;
+  while (!feof(arq) && i < M)
+  {
+    v[i] = cin;
+    cin = le_cliente(arq);
+    i++;
+  }
+
+  if (i != M)
+    M = i;
 }
 
 int menor_valor(Cliente **clientes, int quantidade)
@@ -90,6 +148,8 @@ int menor_valor(Cliente **clientes, int quantidade)
 
 void gerir_reservatorio(Cliente **clientes, int quantidade, FILE *nome_arquivo_entrada, FILE *nome_arquivo_saida) 
 {
+  // TODO: Inserir aqui o codigo do algoritmo de geracao de particoes
+
     int i;
     Cliente *novo_cliente;
 
@@ -203,6 +263,5 @@ void selecao_com_substituicao(char *nome_arquivo_entrada, Nomes *nome_arquivos_s
 
 void selecao_natural(char *nome_arquivo_entrada, Nomes *nome_arquivos_saida, int M, int n)
 {
-	//TODO: Inserir aqui o codigo do algoritmo de geracao de particoes
+  // TODO: Inserir aqui o codigo do algoritmo de geracao de particoes
 }
-
--- geracao_particoes.h
+++ geracao_particoes.h
@@ -18,6 +18,9 @@ void classificacao_interna(char *nome_arquivo_entrada, Nomes *nome_arquivos_said
 // nome_arquivo_saida: lista encadeada com nomes de arquivos de saida
 // M: tamanho do array em memoria para manipula��o dos registros
 void selecao_com_substituicao(char *nome_arquivo_entrada, Nomes *nome_arquivos_saida, int M);
+void guarda_no_arquivo(Cliente **v, int i, FILE *p);
+void cria_particao(char *nome_particao, Nomes *nome_arquivos_saida, Cliente **v, FILE *p, int requisita_funcao, int menor, int i);
+void carrega_registros(Cliente *v[], FILE *arq, int M);
 
 // Executa o algoritmo de geracao de particoes por Selecao Natural
 // nome_arquivo_entrada: nome do arquivo de entrada
